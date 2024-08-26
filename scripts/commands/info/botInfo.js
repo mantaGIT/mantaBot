117696acc418c84eb43f6bea28ec7667c99b2986
manta-sp <manta.splatoon@gmail.com>
2024-08-26 23:29:53 +0900

    apply prettier rules

--- scripts/commands/info/botInfo.js
+++ scripts/commands/info/botInfo.js
@@ -1,14 +1,13 @@
-const { SlashCommandBuilder } = require('discord.js');
-const botInfoEmbedBuilder = require('../../../scripts/reply-builders/botInfo-embedBuilder.js');
-
+const { SlashCommandBuilder } = require("discord.js");
+const botInfoEmbedBuilder = require("../../../scripts/reply-builders/botInfo-embedBuilder.js");
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('만타봇')
-		.setDescription('만타봇 정보를 알려줍니다.'),
-	async execute(interaction) {
-		await interaction.deferReply({ ephemeral: true });
-		const botInfoEmbed = await botInfoEmbedBuilder.botInfoEmbedBuilder();
-		await interaction.editReply(botInfoEmbed);
-	},
+    data: new SlashCommandBuilder()
+        .setName("만타봇")
+        .setDescription("만타봇 정보를 알려줍니다."),
+    async execute(interaction) {
+        await interaction.deferReply({ ephemeral: true });
+        const botInfoEmbed = await botInfoEmbedBuilder.botInfoEmbedBuilder();
+        await interaction.editReply(botInfoEmbed);
+    },
 };
--- scripts/data/schedule-data-handler.js
+++ scripts/data/schedule-data-handler.js
@@ -1,55 +1,76 @@
-const _ = require('lodash');
-const fs = require('node:fs');
-const path = require('node:path');
+const _ = require("lodash");
+const fs = require("node:fs");
+const path = require("node:path");
 // eslint-disable-next-line no-undef
 const mainPath = path.dirname(path.dirname(__dirname));
 
 function Schedule(nodeNum, mode, st, et, rule, stages) {
-	this.node = nodeNum;
-	this.mode = mode;
-	this.startTime = st;
-	this.endTime = et;
-	this.rule = rule;
-	this.stages = stages;
-};
+    this.node = nodeNum;
+    this.mode = mode;
+    this.startTime = st;
+    this.endTime = et;
+    this.rule = rule;
+    this.stages = stages;
+}
 
 module.exports = {
-	createScheduleJsonFile(apiData, gamemode) {
-		const nodes = _.get(apiData, gamemode.tag).nodes;
-		const startTimes = _.map(nodes, 'startTime');
-		const endTimes = _.map(nodes, 'endTime');
-		let setting = _.map(nodes, gamemode.setting);
-		if (gamemode.mode !== undefined) {
-			setting = _.filter(_.flattenDeep(setting), ['bankaraMode', gamemode.mode]);
-		}
-		const rules = _.map(setting, 'vsRule');
-		const stagesArr = _.map(setting, 'vsStages');
+    createScheduleJsonFile(apiData, gamemode) {
+        const nodes = _.get(apiData, gamemode.tag).nodes;
+        const startTimes = _.map(nodes, "startTime");
+        const endTimes = _.map(nodes, "endTime");
+        let setting = _.map(nodes, gamemode.setting);
+        if (gamemode.mode !== undefined) {
+            setting = _.filter(_.flattenDeep(setting), [
+                "bankaraMode",
+                gamemode.mode,
+            ]);
+        }
+        const rules = _.map(setting, "vsRule");
+        const stagesArr = _.map(setting, "vsStages");
 
-		const data = Array.from({ length: nodes.length })
-			.map((x, i) => x = new Schedule(i, gamemode.id, startTimes[i], endTimes[i], rules[i], stagesArr[i]));
+        const data = Array.from({ length: nodes.length }).map(
+            (x, i) =>
+                (x = new Schedule(
+                    i,
+                    gamemode.id,
+                    startTimes[i],
+                    endTimes[i],
+                    rules[i],
+                    stagesArr[i],
+                )),
+        );
 
-		const dataFilePath = path.join(mainPath, `resources/data/schedules/${gamemode.id}.json`);
-		fs.writeFileSync(dataFilePath, JSON.stringify(data, null, 2));
-	},
-	loadScheduleJson(mode) {
-		const scheduleFilePath = path.join(mainPath, `resources/data/schedules/${mode.id}.json`);
+        const dataFilePath = path.join(
+            mainPath,
+            `resources/data/schedules/${gamemode.id}.json`,
+        );
+        fs.writeFileSync(dataFilePath, JSON.stringify(data, null, 2));
+    },
+    loadScheduleJson(mode) {
+        const scheduleFilePath = path.join(
+            mainPath,
+            `resources/data/schedules/${mode.id}.json`,
+        );
 
-		try {
-			const schedulesFile = fs.readFileSync(scheduleFilePath);
-			const schedules = JSON.parse(schedulesFile);
-			return schedules;
-		}
-		catch (error) {
-			console.error(error);
-			return undefined;
-		}
-	},
-	getScheduleNow(schedules) {
-		const timeNow = Date.now();
-		const scheduleNow = schedules.find((schedule) => timeNow >= new Date(schedule.startTime) && timeNow < new Date(schedule.endTime));
-		return scheduleNow;
-	},
-	getScheduleByNode(schedules, node) {
-		return schedules.find((schedule) => schedule.node === node);
-	},
+        try {
+            const schedulesFile = fs.readFileSync(scheduleFilePath);
+            const schedules = JSON.parse(schedulesFile);
+            return schedules;
+        } catch (error) {
+            console.error(error);
+            return undefined;
+        }
+    },
+    getScheduleNow(schedules) {
+        const timeNow = Date.now();
+        const scheduleNow = schedules.find(
+            (schedule) =>
+                timeNow >= new Date(schedule.startTime) &&
+                timeNow < new Date(schedule.endTime),
+        );
+        return scheduleNow;
+    },
+    getScheduleByNode(schedules, node) {
+        return schedules.find((schedule) => schedule.node === node);
+    },
 };
