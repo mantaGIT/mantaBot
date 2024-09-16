c5d7c07ff02ee8c44de544a5a5379c11d1bbefdf
manta-sp <manta.splatoon@gmail.com>
2024-09-16 20:30:39 +0900

    add a new slash command "/만타봇"

---
+++ scripts/commands/info/botInfo.js
@@ -0,0 +1,14 @@
+const { SlashCommandBuilder } = require('discord.js');
+const botInfoEmbedBuilder = require('../../../scripts/reply-builders/botInfo-embedBuilder.js');
+
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('만타봇')
+		.setDescription('만타봇 정보를 알려줍니다.'),
+	async execute(interaction) {
+		await interaction.deferReply({ ephemeral: true });
+		const botInfoEmbed = await botInfoEmbedBuilder.botInfoEmbedBuilder();
+		await interaction.editReply(botInfoEmbed);
+	},
+};
